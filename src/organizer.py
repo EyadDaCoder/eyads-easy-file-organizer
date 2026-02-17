9b3c80b03445f894b217399352ed491544461a33
Eyad Taha <eyadta84@gmail.com>
2026-02-17 08:14:20 +0400

    Bugfix 1
    BUGS FIXED:
    
    Correctly handled input
    Fixed how the cache is built

--- src/organizer.py
+++ src/organizer.py
@@ -4,25 +4,16 @@ class organizer:
     def __init__(self):
         print("Eyads Easy File Organizer")
         self.keywords = []
-        self.file_extensions = {
-            "image": [],
-            "video": [],
-            "audio": [],
-            "languages": [],
-            "markup": [],
-            "special text": [],
-            "config text": [],
-            "binaries": []
-        }
         delay(2)
         self.directory = input("1: Enter the directory of the folder you want to organize -: ")
-        self.type = input("2: Enter 'A' if you want to sort by filetype or 'B' if you want to\nsort by keywords -:")
-        if input == "B":
-            keywords_raw = input("3: Enter a list of keywords seperated by backticks -:")
+        self.type = input("2: Enter 'A' if you want to sort by filetype or 'B' if you want to sort by keywords -: ")
+        if self.type == "B":
+            keywords_raw = input("3: Enter a list of keywords seperated by semicolons -:")
             cache = []
             for i in keywords_raw:
                 cache.append(i)
-                if '`' in cache:
-                    cache.remove('`')
-                    self.keywords.append(cache)
-        print("Setup complete!")
+                if ';' in cache:
+                    cache.remove(';')
+                    self.keywords.append(''.join(cache))
+                    cache = []
+        print(f"Setup complete!")
